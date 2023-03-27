ae1be3ca1d019dcd07bb83ca2b3b42b6dba587b7
Gustavo <gustavo.gonda@gmail.com>
2023-03-26 21:29:41 -0500

    Added interface

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import {iCommit} from './interface/commit.interface';
 
 const access_token = "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"
 
 
 @Injectable()
 export class AppService {
-  async getHello(): Promise<any> {
+  async getHello(): Promise<iCommit> {
     try {
       const response = await axios.get('https://api.github.com/repos/soygustavogonzales/fulltimeforce/commits', {
         headers: {
@@ -14,11 +15,9 @@ export class AppService {
         }
       })
   
-      const data = response.data.map( e => {
+      const data = response.data.map( (e:any) => {
         return {
           author_name: e.commit.author.name,
-          //author_profile_url: e.author.html_url || "",
-          //author_avatar_url: e.committer.avatar_url || "",
           commit_message: e.commit.message,
           commit_url: e.commit.url,
           date: e.commit.author.date
---
+++ src/interface/commit.interface.ts
@@ -0,0 +1,6 @@
+export interface iCommit {
+    author_name: string,
+    commit_message: string,
+    commit_url: string,
+    date: string
+}
